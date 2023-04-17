973310fd0cacb4b436ab4f7f75f245b9bdccdb45
Paulo Henrique Gonçalves de Oliveira <paulo.oliveira.phgo@gmail.com>
2023-04-17 10:46:23 -0300

    projeto iniciado

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿//1-Criar uma aplicação para uma agência de turismo, no qual deveremos registrar passagens aéreas com os seguintes dados: Nome do passageiro, Origem, Destino e Data do Voo de 5 passageiros.
+//Antes de entrar no sistema faça um esquema do qual o usuário só possa acessar o menu se a senha for igual à 123456.
+//O sistema deve exibir um menu com as seguintes opções:
+//1- Cadastrar passagem
+//2- Listar Passagens
+//0- Sair
+//Ao cadastrar uma passagem ao final o sistema deverá perguntar se gostaria de cadastrar uma nova passagem caso contrário voltar ao menu anterior(S/N).
+//criar um metodo para validar o login. 
+
+String usuario;
+String senha;
+
+//static bool validarSenha ()
+//if (true)
+//{
+    
+//}
+
+
+//Login
+Console.WriteLine($"Bem-Vindo a IANES TURISMO !");
+Console.WriteLine($"Por favor, informe o seu usuário: ");
+usuario = Console.ReadLine();
+Console.WriteLine($"Por favor, informe a sua senha:");
+senha = Console.ReadLine();
+
+//validarSenha();
+
+
+
+
+
